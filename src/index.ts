2b9a6fa07d4055907746f908bdac1622d4d81e65
Anand Raj <arya07050@gmail.com>
2025-06-21 15:49:56 +0530

    room creation logic added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import ServerConfig from './config/serverConfig.js';
 import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
+import roomHandler from './handlers/roomHandler.js'; // import the room handler to manage room creation and joining
 
 const app = express();
 
@@ -19,11 +20,12 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
     console.log("New user connected");
+    roomHandler(socket); // pass the socket connection to the room handler for room creation and joining
 
     socket.on("disconnected", () => {
         console.log("User disconnected");
-    })
-})
+    });
+});
 
 server.listen(ServerConfig.PORT, () => {
     console.log(`Server is running at port ${ServerConfig.PORT}`);
